413cdc56b4f1d264b8d75b9c3b18ad288658830b
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-23 00:24:35 -0800

    images for label

--- image as background.py
+++ image as background.py
@@ -2,8 +2,15 @@ from tkinter import *
 root = Tk()
 root.title("<TITLE>")
 root.geometry("1015x570")
+from PIL import ImageTk,Image
+canvas = Canvas(root, width = 300, height = 300)
+canvas.pack()
+img = ImageTk.PhotoImage(Image.open("ball.png"))
+canvas.create_image(20, 20, anchor=NW, image=img)
+root.mainloop()
+
  
-bg = PhotoImage(file  = "Folder/file")
+# bg = PhotoImage(file  = "test/images/ball.png")
 #bg = PhotoImage(file  = "Folder/file")
 
 root.mainloop()
